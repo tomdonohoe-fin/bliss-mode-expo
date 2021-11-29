d2e0d09f56ccdd92a30078fbb5ed302f2d700a9b
Tom Donohoe <tom.donohoe@finder.com>
2021-11-30 09:40:09 +1100

    chore: first commit

---
+++ components/MobileOnly.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { Text } from 'react-native';
+
+export default function MobileOnly() {
+    return <Text>some text</Text>
+}
---
+++ components/WebOnly.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+
+export default function WebOnly() {
+    return <h1>some text</h1>
+}
