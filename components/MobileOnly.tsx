dfc7dc227dedeccc5522b79bc2efaa35d063921a
Ping Layson <pinglayson@gmail.com>
2021-11-30 12:05:07 +1100

    Update MobileOnly.tsx

--- components/MobileOnly.tsx
+++ components/MobileOnly.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Text } from 'react-native';
-import { useWalletAccounts } from './bliss-mode-core/hooks/useWalletAccounts';
+import { useWalletAccounts } from '../hooks/useWalletAccounts';
 
 export default function MobileOnly() {
 
